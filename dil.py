dc1fa15ae8c3ee50f692da41256030f413961168
Yuriy <bo20202@yandex.ru>
2017-03-24 09:12:02 +0000

    Little improvements

--- dil.py
+++ dil.py
@@ -3,10 +3,10 @@ import requests
 import progressbar
 import time
 from bs4 import BeautifulSoup
-from PIL import Image
 from io import BytesIO
 from concurrent.futures import ProcessPoolExecutor
 import concurrent.futures
+import sys
 
 
 URL = 'http://dilbert.com/strip/'
@@ -28,17 +28,19 @@ def find_image_url(page):
 
 
 def download_image_bytes(img_url):
-    r = requests.get(img_url)
-    return r.content
+    r = requests.get(img_url, stream=True)
+    return r
 
 
-def save_image(img_bytes, date):
-    image = Image.open(BytesIO(img_bytes))
-    image_name = construct_image_name('dilbert', date, image.format)
-    image.save(image_name, image.format)
+def save_image(request_stream, date):
+    image_name = construct_image_name('dilbert', date, request_stream.headers['content-type'])
+    with open(image_name, 'wb') as out_stream:
+        for chunk in request_stream:
+            out_stream.write(chunk)
 
 
-def construct_image_name(path, date, format):
+def construct_image_name(path, date, format_header):
+    format = format_header.split('/')[-1]  
     return "{0}/{1}.{2}".format(path, date, format.lower())
 
 
@@ -46,47 +48,48 @@ def build_url(date):
     return URL + str(date)
 
 days = (FINAL_DATE - INITIAL_DATE).days
-bar = progressbar.ProgressBar(max_value=1000)
+bar = progressbar.ProgressBar(max_value=days)
 
 
 
 def main():
-    initial_date = INITIAL_DATE
-    items = 1
-    while items <= days:
-        with ProcessPoolExecutor() as executor:
-            future_results = []
-            urls = []
-            date = initial_date
-            for i in range(12): #or server is gonna punish me
-                url = build_url(date)
-                task = executor.submit(get_page, url)
-                future_results.append(task)
-                date = date + DAY
+    try:
+        initial_date = INITIAL_DATE
+        for items in range(1, days, 12):
+            with ProcessPoolExecutor() as executor:
+                future_results = []
+                urls = []
+                date = initial_date
+                for i in range(12): #or server is gonna punish me
+                    url = build_url(date)
+                    task = executor.submit(get_page, url)
+                    future_results.append(task)
+                    date = date + DAY
+                    
+                date = INITIAL_DATE
+                print('##### Processing URLs... #####')
+                for future in concurrent.futures.as_completed(future_results):
+                    img_url = find_image_url(future.result())
+                    urls.append(img_url)
+                    date = date + DAY
                 
-            date = INITIAL_DATE
-            print('##### Processing URLs... #####')
-            for future in concurrent.futures.as_completed(future_results):
-                img_url = find_image_url(future.result())
-                urls.append(img_url)
-                date = date + DAY
-            
-            future_results = []
-            
-            for url in urls:
-                task = executor.submit(download_image_bytes, url)
-                future_results.append(task)
-            
-            date = initial_date
-            print('##### Downloading images #####')
-            for future in concurrent.futures.as_completed(future_results):
-                save_image(future.result(), date)
-                date = date + DAY
-            executor.shutdown()
-        initial_date = date
-        bar.update(items)
-        items += 12
-        time.sleep(2)
+                future_results = []
+                
+                for url in urls:
+                    task = executor.submit(download_image_bytes, url)
+                    future_results.append(task)
+                
+                date = initial_date
+                print('##### Downloading images #####')
+                for future in concurrent.futures.as_completed(future_results):
+                    save_image(future.result(), date)
+                    date = date + DAY
+                executor.shutdown()
+            initial_date = date
+            bar.update(items)
+            time.sleep(2)
+    except:
+        print("Error!", sys.exc_info()[0])
 
 
 if __name__ == '__main__':
